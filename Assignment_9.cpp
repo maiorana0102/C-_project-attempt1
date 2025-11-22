80332a1e6f5256b23767ca6ef3e66d08de9099b6
maiorana0102 <maiorana1028@gmail.com>
2025-11-22 02:48:11 -0500

    Add files via upload

--- Assignment_9.cpp
+++ Assignment_9.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <limits>
 using namespace std;
 //
 // lets set up the global variables we will use boolean to determine if the input was hex or not
 //globals
 string user_input;
+string final_output;
+bool is_hex;
+test 
